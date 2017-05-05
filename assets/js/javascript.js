e93ac2148ca9a6876e83cb95d0dd4eee28b17587
“Laura <laureen.pelaez.isaza@gmail.com>
2017-05-05 17:59:07 -0300

    create button and textArea

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,43 +1,52 @@
 // MIEMBROS TIENEN QUE SER GLOBALES Y LOS COMENTARIOS DEBEN SER GLOBALES
-// CREAR LA FUNCION DE QUE SE PUEDA CREAR COMENTARIOS POR CADA UNO DE LOS MUESTRO
-var comentarios= [];
-var contenedor = document.getElementById('contenedor')
-function MiembroSquad (nombre, apellido, edad, hobbies) { // constructor creado con las propiedades solicitadas
-  this.nombre = nombre,
-  this.apellido = apellido,
-  this.edad = edad,
-  this.hobbies = hobbies, // debe ser un array porque existen varios hobbies
-  this.photos = new Image();//necesito que por cada persona me muestre su foto
-  this.photos.src="photos/" + this.nombre + ".jpg"  ;///de donde saca la imagen?
-  
+// CREAR LA FUNCION DE QUE SE PUEDA CREAR COMENTARIOS POR CADA UNO DE Las personas
 
-  
+var contenedor = document.getElementById('contenedor')
+
+function MiembroSquad(nombre, apellido, edad, hobbies) { // constructor creado con las propiedades solicitadas
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.hobbies = hobbies; // debe ser un array porque existen varios hobbies
+    this.photos = new Image(); //necesito que por cada persona me muestre su foto
+    this.photos.src = "photos/" + this.nombre + ".jpg"; ///de donde saca la imagen?
 };
 
-function inicializar () {
-  var teamMates = [new MiembroSquad('Orieta', 'Toro', '27', ['Leer', 'dormir']),
-    new MiembroSquad('Constanza', 'Pecori', '26', ['tejer', 'comer', 'dormir ']),
-    new MiembroSquad('Rangi', 'Becerra', '32', ['viajar']),
-    new MiembroSquad('Caterina', 'Da Silva', '26', ['juegos online']),
-    new MiembroSquad('Rosa', 'Díaz', '32', ['Pensar en la inmortalidad del cangrejo']),
-    new MiembroSquad('Genesis', 'Narváez', '27', ['pasear en bici']),
-    new MiembroSquad('Laura', 'Peláez', 'undefined', ['cinéfila']),
-    new MiembroSquad('Camila', 'González', '26', ['ver netflix', 'jugar con mascotas', 'degustar cervezas'])];
+function Comentario(id_miembro, comentario, likes) {
+    this.id_miembro = id_miembro;
+    this.comentario = comentario;
+    this.likes = likes;
+};
 
-  console.log(teamMates)
-  var html = '' // creo esta variable para que me agrupe un bloque de codigo html y me lo ingrese al contenedor
-  for (var i = 0; i < teamMates.length; i++) { // recorro mi array teamMates que me contiene las nuevas instancias
-    html += '<section> <img width=200 src="' + teamMates[i].photos.src + '" > </br> <strong> Nombre: </strong>' + // += para que concatene las cosas y me muestre todo
-    teamMates[i].nombre + '</br>' + ' <strong> Apellido: </strong>' +
-    teamMates[i].apellido + '</br>' + '<strong> edad: </strong>' +
-    teamMates[i].edad + '</br>'+ "Hobbies : "
+function inicializar() {
+    var teamMates = [new MiembroSquad('Orieta', 'Toro', '27', ['Leer', 'dormir']),
+        new MiembroSquad('Constanza', 'Pecori', '26', ['tejer', 'comer', 'dormir ']),
+        new MiembroSquad('Rangi', 'Becerra', '32', ['viajar']),
+        new MiembroSquad('Caterina', 'Da Silva', '26', ['juegos online']),
+        new MiembroSquad('Rosa', 'Díaz', '32', ['Pensar en la inmortalidad del cangrejo']),
+        new MiembroSquad('Genesis', 'Narváez', '27', ['pasear en bici']),
+        new MiembroSquad('Laura', 'Peláez', 'undefined', ['cinéfila']),
+        new MiembroSquad('Camila', 'González', '26', ['ver netflix', 'jugar con mascotas', 'degustar cervezas'])
+    ];
 
-    html += '<ul>' // porque necesito crear un  ul q me meta los li
+    var html = '' // creo esta variable para que me agrupe un bloque de codigo html y me lo ingrese al contenedor
+    for (var i = 0; i < teamMates.length; i++) { // recorro mi array teamMates que me contiene las nuevas instancias
+        html += '<section> <img width=200 src="' + teamMates[i].photos.src + '" > </br> <strong> Nombre: </strong>' + // += para que concatene las cosas y me muestre todo
+            teamMates[i].nombre + '</br>' + ' <strong> Apellido: </strong>' +
+            teamMates[i].apellido + '</br>' + '<strong> edad: </strong>' +
+            teamMates[i].edad + '</br>' + "Hobbies : "
 
-    for (var j = 0; j < teamMates[i].hobbies.length; j++) { // como hobbies es un array de varias cosas, necesito recorrerlo para buscar el hobbie
-      html += '<li>' + teamMates[i].hobbies[j] + '</li>'
+        html += '<ul>' // porque necesito crear un  ul q me meta los li
+
+        for (var j = 0; j < teamMates[i].hobbies.length; j++) { // como hobbies es un array de varias cosas, necesito recorrerlo para buscar el hobbie
+            html += '<li>' + teamMates[i].hobbies[j] + '</li>'
+        }
+        html += "</ul>";
+        html += "<textarea></textarea></br>";
+        html += '<button type="button">Dejar Comentario!</button>';
+        html += "</section><br/>";
+
+
     }
-    html += '</ul></section><br/>'
-  }
-  contenedor.innerHTML = html
+    contenedor.innerHTML = html
 }
